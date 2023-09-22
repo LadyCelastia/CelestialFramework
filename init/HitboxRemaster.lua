c5923fdb123ae3ad3b07ccfb6a288a0959a28c52
LadyCelestia <140333669+LadyCelastia@users.noreply.github.com>
2023-09-22 23:25:16 +0800

    Update and rename HitboxRemaster.luau to HitboxRemaster.lua

--- init/HitboxRemaster.lua
+++ init/HitboxRemaster.lua
@@ -28,33 +28,30 @@ export type HitboxType = {
     Definitions
 --]]
 local HttpService = game:GetService("HttpService")
+local DebugMode = true
 
 --[[
     @function
     Universal functions
 --]]
+-- Debug mode for stack tracer
+local function getStackLevel(): number
+	if DebugMode == true then
+		return 1
+	end
+	return 2
+
+end
+
 -- Concatenate string prefix for console output
 local function concatPrint(String: string): string
     return "[" .. script.Name .. "]: " .. String
 end
 
--- Returns a deep copy of a table
-local function DeepCopy(Table: {any}): {any}
-    local newTable = {}
-    for i,v in pairs(Table) do
-        if typeof(v) ~= "table" then
-            newTable[i] = v
-        else
-            newTable[i] = DeepCopy(v)
-        end
-    end
-    return newTable
-end
-
 -- Find an index/value or index-value pair in Table and all its children tables
 local function DeepFind(Table: {any}, Row: Pair<any, any>): Pair<any, any> | nil
     if #Row ~= 1 then
-        error(concatPrint("Row argument of function DeepFind may only have one key-value pair.", 2))
+        error(concatPrint("Row argument of function DeepFind may only have one key-value pair."), 2)
     end
     for i,v in pairs(Table) do
         if typeof(v) ~= "table" then
@@ -151,41 +148,41 @@ end
 
 --[[
     @class Enum
-    Enum implementation to prevent unexpected values
+    Enumeration implementation to prevent unexpected values
 --]]
 local enum = {}
 
 enum._new = function(Name: string, Children: {string: any}): enumPair<any>
-    if enum[Name] ~= nil then
-        return enum[Name]
-    end
+	if enum[Name] ~= nil then
+		return enum[Name]
+	end
 
-    enum[Name] = Children
+	enum[Name] = Children
 
-    return setmetatable(Children, {
-        __index = function(_, i): any
-            return enum[i] or error(concatPrint(i .. " is not a valid member of the Enum.", 2))
-        end,
-        __newindex = function(): ()
-            error(concatPrint("Enums are read-only.", 2))
-        end
-    })
+	return setmetatable(Children, {
+		__index = function(_, i): any
+			return enum[i] or error(concatPrint(i .. " is not a valid member of the Enum."), getStackLevel())
+		end,
+		__newindex = function(): ()
+			error(concatPrint("Enums are read-only."), getStackLevel())
+		end
+	})
 end
 
 enum._get = function(Name: string): enumPair<any>
-    if enum[Name] then
-        return {Name = enum[Name]}
-    else
-        return DeepFind(enum, {Name = nil})
-    end
+	if enum[Name] then
+		return {Name = enum[Name]}
+	else
+		return DeepFind(enum, {Name = nil})
+	end
 end
 
 -- Do not call _isEnum as a method as self is readonly
 enum._isEnum = function(ValueToCheck: any): boolean
-    if IsPrivate(ValueToCheck) == false and DeepFind(enum, {ValueToCheck}) ~= nil then
-        return true
-    end
-    return false
+	if IsPrivate(ValueToCheck) == false and DeepFind(enum, {ValueToCheck}) ~= nil then
+		return true
+	end
+	return false
 end
 
 enum._new("StateEnum", {Active = "Active", Paused = "Paused", Dead = "Dead"})
@@ -193,156 +190,181 @@ enum._new("ConstructionMode", {None = nil, Linear = "Linear", Bezier = "Bezier"}
 
 --[[
     @class ScriptConnection
-    Child of @ScriptSignal
+    Child of @ConnectionRunner
 --]]
 local Connection = {}
 Connection.__index = Connection
 Connection.StateEnum = enum._get("StateEnum")
 
 Connection.new = function(signal: ScriptSignal, func: any, once: boolean?): ScriptConnection
-    local self = setmetatable({}, Connection)
+	local self = setmetatable({}, Connection)
 
-    self._Connected = true
-    self._Signal = signal
-    self._Function = func
-    self._Identifier = HttpService:GenerateGUID(false)
-    self._Once = false
-    self._State = enum.StateEnum.Paused
+	self._Connected = true
+	self._Signal = signal
+	self._Function = func
+	self._Identifier = HttpService:GenerateGUID(false)
+	self._Once = false
+	if once ~= nil then
+		self._Once = once
+	end
+	self._State = enum.StateEnum.Paused
 
-    return self
+	return self
 end
 
 function Connection:_Fire(...): ()
-    if self._Connected == true then
-        coroutine.wrap(self._Function(...))
-        if self._Once == true then
-            self:Disconnect()
-        end
-    end
+	if self._Connected == true then
+		coroutine.wrap(self._Function)(...)
+		if self._Once == true then
+			self:Disconnect()
+		end
+	end
+end
+
+function Connection:GetIdentifier(): string
+	return self._Identifier
 end
 
 function Connection:Disconnect(): ()
-    self._Connected = false
-    self._Function = nil
-    self._State = enum.StateEnum.Dead
+	self._Connected = false
+	self._Function = nil
+	self = {_State = enum.StateEnum.Dead}
 end
 
 --[[
-    @class Runner
+    @class ConnectionRunner
+    @parent ScriptConnection
     Private child of @ScriptSignal, manages child ScriptConnections
 --]]
 local Runner = {}
 Runner.__index = Runner
 Runner.StateEnum = enum._get("StateEnum")
 
-Runner.new = function(): RunnerType
-    local self = setmetatable({}, Runner)
+Runner.new = function(): ConnectionRunner
+	local self = setmetatable({}, Runner)
 
-    self._Connections = {}
-    self._State = enum.StateEnum.Active
+	self._Connections = {}
+	self._State = enum.StateEnum.Active
 
-    return self
+	return self
 end
 
 function Runner:_AddConnection(Cn: ScriptConnection): ()
-    table.insert(self._Connections, Cn)
+	table.insert(self._Connections, Cn)
 end
 
 function Runner:_CleanUp(): ()
-    local CullingList = {}
-    for i,v in ipairs(self._Connections) do
-        if v._Connected == false then
-            table.insert(CullingList, i)
-        end
-    end
-    CullTable(self._Connections, CullingList)
+	local CullingList = {}
+	for i,v in ipairs(self._Connections) do
+		if v._Connected == false then
+			table.insert(CullingList, i)
+		end
+	end
+	CullTable(self._Connections, CullingList)
 end
 
 function Runner:_RemoveConnection(identifier: string): ()
-    for _,v in ipairs(self._Connections) do
-        if v._Identifier == identifier then
-            v:Disconnect()
-        end
-    end
-    self:_CleanUp()
+	for _,v in ipairs(self._Connections) do
+		if v._Identifier == identifier then
+			v:Disconnect()
+		end
+	end
+	self:_CleanUp()
 end
 
 function Runner:_GetConnections(): Pair<number, ScriptConnection>
-    return self._Connections
+	return self._Connections
+end
+
+function Runner:_FireOne(identifier: string, args: {any}): ()
+	for _,v in ipairs(self._Connections) do
+		if v._Identifier == identifier then
+			v:Fire(table.unpack(args))
+		end
+	end
 end
 
 function Runner:_FireAll(...): ()
-    for _,v in ipairs(self._Connections) do
-        v:_Fire(...)
-    end
+	for _,v in ipairs(self._Connections) do
+		v:_Fire(...)
+	end
 end
 
 function Runner:Destroy(): ()
-    for _,v in ipairs(self._Connections) do
-        v:Disconnect()
-    end
-    self:_CleanUp()
-    table.clear(self)
-    self._State = enum.StateEnum.Dead
+	for _,v in ipairs(self._Connections) do
+		v:Disconnect()
+	end
+	self:_CleanUp()
+	self = {_State = enum}
 end
 
 --[[
     @class ScriptSignal
-    @parent ScriptConnection
+    @parent enum, ConnectionRunner
     ScriptSignal implementation
 --]]
 local Signal = {}
 Signal.__index = Signal
+Signal.StateEnum = enum._get("StateEnum")
 
 Signal.new = function(): ScriptSignal
-    local self = setmetatable({}, Signal)
+	local self = setmetatable({}, Signal)
 
-    self._ActiveRunner = Runner.new()
-    self._State = enum.StateEnum.Active
+	self._ActiveRunner = Runner.new()
+	self._State = enum.StateEnum.Active
 
-    return self
+	return self
 end
 
 function Signal:GetState(): string
-    return self._State
+	return self._State
 end
 
-function Signal:Fire(): ()
-    self._ActiveRunner:_FireAll()
+function Signal:FireOne(...): ()
+	self._ActiveRunner:_FireOne(...)
+end
+
+function Signal:Fire(...): ()
+	self._ActiveRunner:_FireAll(...)
 end
 
 function Signal:Connect(func: any, connectImmediately: boolean?): ScriptConnection
-    connectImmediately = connectImmediately or true
-    local newConnection = Connection.new(self, func)
+	if connectImmediately == nil then
+		connectImmediately = true
+	end
+	local newConnection = Connection.new(self, func)
 
-    if connectImmediately == true then
-        self._ActiveRunner:_AddConnection(newConnection) 
-    end
+	if connectImmediately == true then
+		self._ActiveRunner:_AddConnection(newConnection) 
+	end
 
-    return newConnection
+	return newConnection
+end
+
+function Signal:DisconnectOne(identifier: string): ()
+	self._ActiveRunner:_RemoveConnection(identifier)
 end
 
 function Signal:DisconnectAll(): ()
-    for _,v in ipairs(self._ActiveRunner:_GetConnections()) do
-        v:Disconnect()
-    end
-    self._ActiveRunner:_CleanUp()
+	for _,v in ipairs(self._ActiveRunner:_GetConnections()) do
+		v:Disconnect()
+	end
+	self._ActiveRunner:_CleanUp()
 end
 
 function Signal:Once(func: any): ScriptConnection
-    local newConnection = self:Connect(func, false)
-    newConnection._Once = true
+	local newConnection = self:Connect(func, false)
+	newConnection._Once = true
 
-    self._ActiveRunner:_AddConnection(newConnection)
+	self._ActiveRunner:_AddConnection(newConnection)
 
-    return newConnection
+	return newConnection
 end
 
 function Signal:Destroy(): ()
-    self:DisconnectAll()
-    self._ActiveRunner:Destroy()
-    table.clear(self)
-    self._State = enum.StateEnum.Dead
+	self:DisconnectAll()
+	self._ActiveRunner:Destroy()
+	self = {_State = enum.StateEnum.Dead}
 end
 
 --[[
